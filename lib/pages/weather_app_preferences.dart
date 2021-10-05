48c06f972a67fc0c8038f91feaca282f32c601cb
Pechenyice <german.iskhakov@mail.ru>
2021-10-06 02:07:00 +0300

    added preferences layout

--- lib/pages/weather_app_preferences.dart
+++ lib/pages/weather_app_preferences.dart
@@ -6,11 +6,10 @@ class WeatherAppPreferences extends StatefulWidget {
 }
 
 class _WeatherAppPreferencesState extends State<WeatherAppPreferences> {
+  List cities = ['Москва', 'Санкт-Петербург'];
+
   @override
   Widget build(BuildContext context) {
-
-    List cities = ['Москва', 'Санкт-Петербург'];
-
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Color(0xFFE2EBFF),
@@ -27,25 +26,43 @@ class _WeatherAppPreferencesState extends State<WeatherAppPreferences> {
           ),
         ),
       ),
-      body: ListView.separated(
-        itemCount: cities.length,
-        separatorBuilder: (BuildContext context, int index) => Divider(),
-        itemBuilder: (BuildContext context, int index) {
-          String word = cities[index];
+      body: Container(
+        decoration: BoxDecoration(color: Color(0xFFE2EBFF)),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          child: ListView.separated(
+            itemCount: cities.length,
+            separatorBuilder: (BuildContext context, int index) => Divider(),
+            itemBuilder: (BuildContext context, int index) {
+              String word = cities[index];
 
-          return ListTile(
-            title: Text(word),
-            trailing: Icon(
-              Icons.delete_forever,
-              color: Colors.black,
-            ),
-            onTap: () {
-              setState(() {
-                cities.removeAt(index);
-              });
+              return Container(
+                decoration: BoxDecoration(
+                    color: Color(0xFFDEE9FF),
+                    borderRadius: BorderRadius.all(Radius.circular(12)),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black.withOpacity(0.8),
+                        spreadRadius: -6.0,
+                        blurRadius: 12.0,
+                      ),
+                    ]),
+                child: ListTile(
+                  title: Text(word),
+                  trailing: Icon(
+                    Icons.delete_forever,
+                    color: Colors.black,
+                  ),
+                  onTap: () {
+                    setState(() {
+                      cities.removeAt(index);
+                    });
+                  },
+                ),
+              );
             },
-          );
-        },
+          ),
+        ),
       ),
     );
   }
