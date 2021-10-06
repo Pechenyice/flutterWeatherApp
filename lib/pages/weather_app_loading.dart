d38e420f1ceb15b8dd0176b3984da6138be5b97d
Pechenyice <german.iskhakov@mail.ru>
2021-10-06 20:41:10 +0300

    added preferences data, async fetching and route data

--- lib/pages/weather_app_loading.dart
+++ lib/pages/weather_app_loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class WeatherAppLoading extends StatefulWidget {
   const WeatherAppLoading({Key? key}) : super(key: key);
@@ -8,6 +11,35 @@ class WeatherAppLoading extends StatefulWidget {
 }
 
 class _WeatherAppLoadingState extends State<WeatherAppLoading> {
+
+  List weather = [];
+  String currentCity = 'Санкт-Петербург';
+
+  Future<void> getWeather() async {
+    Response response = await get(Uri.parse('http://api.openweathermap.org/data/2.5/forecast?q=$currentCity&cnt=6&appid=f6bd006ca2e0d7841a2d086ff08db0fe'));
+    Map data = jsonDecode(response.body);
+    weather = data['list'];
+    Navigator.pushReplacementNamed(context, '/', arguments: {
+      'weather': weather
+    });
+  }
+
+  Future<void> initPrefs() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    currentCity = prefs.getString('activeCity') ?? 'Санкт-Петербург';
+  }
+
+  void init() async {
+    await initPrefs();
+    await getWeather();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    init();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
