e530153500d63b15fd1c868f4d6eaeca9aa40107
Pechenyice <german.iskhakov@mail.ru>
2021-10-27 00:43:40 +0300

    created image variables

---
+++ lib/helpers/ThemeImages.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class ThemeImages {
+  ThemeImages._();
+  static AssetImage background = AssetImage("assets/imgs/background_light.png");
+}
