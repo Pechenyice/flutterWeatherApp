911aa0112ded92687f34c7c7f9a29cd36e33246c
Pechenyice <german.iskhakov@mail.ru>
2021-10-27 00:43:29 +0300

    created color variables

--- lib/helpers/ThemeColors.dart
+++ lib/helpers/ThemeColors.dart
@@ -3,5 +3,11 @@ import 'package:flutter/material.dart';
 
 class ThemeColors {
   ThemeColors._();
-  static const Color black = Color(0xFF78909C);
+  static Color weatherBackground = Color(0xFFE2EBFF);
+  static Color weatherPreview = Color(0xFFE0E9FD);
+  static Color black = Color(0xFF000000);
+  static Color white = Color(0xFFFFFFF);
+  static Color weekGradientStart = Color(0xFFCDDAF5);
+  static Color weekGradientEnd = Color(0xFF9CBCFF);
+  static Color menuButtons = Color(0xFF0256FF);
 }
