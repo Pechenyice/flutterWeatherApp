1cf4efdcb46f787342e7d890fde7b7e925f3086e
Pechenyice <german.iskhakov@mail.ru>
2021-10-27 00:44:18 +0300

    updated page to theme swap

--- lib/pages/weather_app_loading.dart
+++ lib/pages/weather_app_loading.dart
@@ -1,4 +1,6 @@
 import 'dart:convert';
+import 'package:flutter_weather_app/helpers/ThemeColors.dart';
+import 'package:flutter_weather_app/helpers/ThemeImages.dart';
 import 'package:http/http.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -14,6 +16,7 @@ class _WeatherAppLoadingState extends State<WeatherAppLoading> {
 
   List weather = [];
   String currentCity = 'Санкт-Петербург';
+  bool darkTheme = false;
 
   Future<void> getWeather() async {
     Response response = await get(Uri.parse('http://api.openweathermap.org/data/2.5/forecast?q=$currentCity&cnt=6&appid=f6bd006ca2e0d7841a2d086ff08db0fe'));
@@ -27,6 +30,17 @@ class _WeatherAppLoadingState extends State<WeatherAppLoading> {
   Future<void> initPrefs() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     currentCity = prefs.getString('activeCity') ?? 'Санкт-Петербург';
+    darkTheme = prefs.getStringList('themeSettings')?.elementAt(1) == "1";
+    if (darkTheme) {
+        ThemeColors.black = Colors.white;
+        ThemeColors.white = Colors.black;
+        ThemeColors.weatherBackground = Color(0xFF0C172B);
+        ThemeColors.weatherPreview = Color(0xFF0D182C);
+        ThemeColors.weekGradientStart = Color(0xFF223B70);
+        ThemeColors.weekGradientEnd = Color(0xFF0F1F40);
+        ThemeColors.menuButtons = Color(0xFF0A1743);
+        ThemeImages.background = const AssetImage("assets/imgs/background_dark.png");
+    }
   }
 
   void init() async {
@@ -45,7 +59,7 @@ class _WeatherAppLoadingState extends State<WeatherAppLoading> {
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
-          color: Color(0xFFE2EBFF)
+          color: ThemeColors.weatherBackground
         ),
         child: Column(
           children: [
@@ -55,12 +69,13 @@ class _WeatherAppLoadingState extends State<WeatherAppLoading> {
                 "Weather",
                 style: TextStyle(
                   fontSize: 35.0,
-                  fontWeight: FontWeight.w600
+                  fontWeight: FontWeight.w600,
+                  color: ThemeColors.black
                 ),
               ),
             ),
             SizedBox(height: 120.0,),
-            Text("svg loader: null pointer")
+            Text("svg loader: null pointer", style: TextStyle(color: ThemeColors.black),)
           ],
         ),
       ),
